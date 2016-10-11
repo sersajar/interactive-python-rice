9f0a12fed0b64e512d8663114b70be4d11831771
Sergi Sabate <sersajar@gmail.com>
2016-10-11 18:03:51 +0200

    adding examples

---
+++ aritmeticExprPractice.py
@@ -0,0 +1,19 @@
+
+# Hours, minutes and seconds conversion formula
+
+print (7 * 60 + 21) * 60 + 37
+
+# Rectangle perimeter formula
+print  2 * 4 + 2 * 7
+
+# Circle area formula 
+print 2 * 3.14 * 8
+
+# Future value formula
+print 1000 * (1 + 0.01 * 7) ** 10
+
+# 
+print "My name is " + "Joe" + " " + "Warren."
+
+# Distance formula
+print ((2 - 5) ** 2 + (2 - 6) ** 2) ** 0.5
---
+++ fahrenheit.py
@@ -0,0 +1,14 @@
+# convert  from Fahrenheit to Celsius
+# c = 5/9 * (f - 32)
+
+temp_Fahrenheit = 77
+temp_Celsius = 5.0 / 9.0 * (temp_Fahrenheit - 32)
+
+print temp_Celsius
+
+# convert  from Celsius to Fahrenheit
+
+temp_Celsius = 0
+temp_Fahrenheit = 9.0/5.0 * temp_Celsius + 32
+
+print temp_Fahrenheit
